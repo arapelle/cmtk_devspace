207222871c48677b68c6f706c5c22610851fa242
Aymeric Pellé <aympelle@gmail.com>
2023-04-03 17:13:12 +0200

    v0.1: New architecture for cmtk-cpputil. Use option_or_set_ifdef().

--- test_cpp_projects.sh
+++ test_cpp_projects.sh
@@ -3,8 +3,8 @@
 command rm -rf /tmp/local \
 && command cmake_test_install ./cppexe \
 && command cmake_test_full_install ./cpphutil \
-&& command cmake_test_full_install ./cpputil
-&& echo "SUCCESS" && exit 0
+&& command cmake_test_full_install ./cmtkt-cpputil \
+&& echo "SUCCESS" && bell && exit 0
 
 echo "FAIL"
 exit 1
