c0d1ebcb0903c968a2cd9d4ece03cb4ce8b4bfc9
Aymeric Pellé <aympelle@gmail.com>
2023-05-06 00:32:14 +0200

    v0.2:#2: Projects use the new unit test API. (Fix Windows problem.)

--- test_cpp_projects.sh
+++ test_cpp_projects.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-format="{bold}{grey}{BLUE}"
+format="{bold}{blue}"
 commandf "$format" rm -rf /tmp/local \
 && commandf "$format" cmake_test_install ./cppexe \
 && commandf "$format" cmake_test_full_install ./cpphutil \
