631ec17b051bf9d2ddce77f3f07e9b3898942c14
Aymeric Pellé <aympelle@gmail.com>
2023-05-10 15:09:08 +0200

    v0.2:#2: Rename directory cppexe cmtk_tests-cpp_exe.

--- test_cpp_projects.sh
+++ test_cpp_projects.sh
@@ -2,7 +2,7 @@
 
 format="{bold}{blue}"
 commandf "$format" rm -rf /tmp/local \
-&& commandf "$format" cmake_test_install ./cppexe \
+&& commandf "$format" cmake_test_install ./cmtk_tests-cpp_exe \
 && commandf "$format" cmake_test_full_install ./cmtk_tests-cpp_hutil \
 && commandf "$format" cmake_test_full_install ./cmtk_tests-cpp_util \
 && echo "SUCCESS" && bell -b && exit 0
