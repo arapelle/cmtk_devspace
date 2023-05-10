e2bfe8a37893f3ec9a119dcd3cfa4ee61f83d58b
Aymeric Pellé <aympelle@gmail.com>
2023-05-10 14:50:19 +0200

    v0.2:#2: Rename directory cmtkt-cpputil cmtk_tests-cpp_util.

--- test_cpp_projects.sh
+++ test_cpp_projects.sh
@@ -4,7 +4,7 @@ format="{bold}{blue}"
 commandf "$format" rm -rf /tmp/local \
 && commandf "$format" cmake_test_install ./cppexe \
 && commandf "$format" cmake_test_full_install ./cpphutil \
-&& commandf "$format" cmake_test_full_install ./cmtkt-cpputil \
+&& commandf "$format" cmake_test_full_install ./cmtk_tests-cpp_util \
 && echo "SUCCESS" && bell -b && exit 0
 
 echo "FAIL"
