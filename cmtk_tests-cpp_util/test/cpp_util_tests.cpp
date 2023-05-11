ab48b1c856be7b576a4d41667aceab9660ee850a
Aymeric Pellé <aympelle@gmail.com>
2023-05-12 00:01:10 +0200

    v0.2:#3: Add unit test programs to test all cmtk add_test functions.

--- cmtk_tests-cpp_util/test/cpp_util_tests.cpp
+++ cmtk_tests-cpp_util/test/cpp_util_tests.cpp
@@ -7,9 +7,13 @@ TEST(cpp_util_tests, module_name__cpp_util)
     ASSERT_EQ(cpp_util::module_name(), "cpp_util");
 }
 
-TEST(cpp_util_tests, compute__4_5__20)
+TEST(cpp_util_tests, compute__VALUE_5__5VALUE)
 {
-    ASSERT_EQ(cpp_util::compute(4,5), 20);
+#ifndef UNIT_TEST_VALUE
+    FAIL();
+#else
+    ASSERT_EQ(cpp_util::compute(UNIT_TEST_VALUE, 5), (5 * UNIT_TEST_VALUE));
+#endif
 }
 
 int main(int argc, char** argv)
