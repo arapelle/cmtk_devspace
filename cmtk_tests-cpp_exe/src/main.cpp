68e9e69819c8baaa452bb3f25bfd1b23e86ea4da
Aymeric Pellé <aympelle@gmail.com>
2023-05-12 17:16:06 +0200

    v0.2:#3: cmtk_tests-cpp_exe uses spdlog.
    Depends on #38 (cmtk).

--- cmtk_tests-cpp_exe/src/main.cpp
+++ cmtk_tests-cpp_exe/src/main.cpp
@@ -1,10 +1,14 @@
 #include <cpp_exe/cpp_exe.hpp> 
+#include <spdlog/spdlog.h>
 #include <iostream>
 #include <cstdlib>
 
 int main()
 {
-    std::cout << cmtk_tests::cpp_exe::module_name() << std::endl;
-    std::cout << cpp_exe::module_name() << std::endl;
+    spdlog::set_level(spdlog::level::level_enum(SPDLOG_ACTIVE_LEVEL));
+    SPDLOG_DEBUG("Begin");
+    SPDLOG_INFO("Printing cmtk_tests::cpp_exe::module_name(): {}", cmtk_tests::cpp_exe::module_name());
+    SPDLOG_INFO("Printing cpp_exe::module_name(): {}", cmtk_tests::cpp_exe::module_name());
+    SPDLOG_DEBUG("End");
     return EXIT_SUCCESS;
 }
