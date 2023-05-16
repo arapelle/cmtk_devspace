2f359761e05722cdca2d97e18a897fbd266d6bd3
Aymeric Pellé <aympelle@gmail.com>
2023-05-16 15:42:02 +0200

    v0.2: Change test projects hierarchy.

--- test_cpp_projects.sh
+++ test_cpp_projects.sh
@@ -2,9 +2,9 @@
 
 format="{bold}{blue}"
 commandf "$format" rm -rf /tmp/local \
-&& commandf "$format" cmake_test_install ./cmtk_tests-cpp_exe \
-&& commandf "$format" cmake_test_full_install ./cmtk_tests-cpp_hutil \
-&& commandf "$format" cmake_test_full_install ./cmtk_tests-cpp_util \
+&& commandf "$format" cmake_test_install ./projects/cmtk_tests/cmtk_tests-cpp_exe \
+&& commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_hutil \
+&& commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_util \
 && echo "SUCCESS" && bell -b && exit 0
 
 echo "FAIL"
