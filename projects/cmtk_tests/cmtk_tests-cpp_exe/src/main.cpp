676d36e525d56a0aa3b45db07fa8785874877254
Aymeric Pellé <aympelle@gmail.com>
2023-06-06 18:28:51 +0200

    v0.2:#29: Executable project uses modern API.

--- projects/cmtk_tests/cmtk_tests-cpp_exe/src/main.cpp
+++ projects/cmtk_tests/cmtk_tests-cpp_exe/src/main.cpp
@@ -1,4 +1,5 @@
 #include <cpp_exe/cpp_exe.hpp> 
+#include <cpp_exe/version.hpp> 
 #include <spdlog/spdlog.h>
 #include <iostream>
 #include <cstdlib>
@@ -7,8 +8,8 @@ int main()
 {
     spdlog::set_level(spdlog::level::level_enum(SPDLOG_ACTIVE_LEVEL));
     SPDLOG_DEBUG("Begin");
-    SPDLOG_INFO("Printing cmtk_tests::cpp_exe::module_name(): {}", cmtk_tests::cpp_exe::module_name());
-    SPDLOG_INFO("Printing cpp_exe::module_name(): {}", cmtk_tests::cpp_exe::module_name());
+    SPDLOG_INFO("Printing cmtk_tests::cpp_exe::module_name(): {}-{}", cmtk_tests::cpp_exe::module_name(), CMTK_TESTS_CPP_EXE_VERSION);
+    SPDLOG_INFO("Printing cpp_exe::module_name(): {}-{}", cpp_exe::module_name(), CMTK_TESTS_CPP_EXE_VERSION);
     SPDLOG_DEBUG("End");
     return EXIT_SUCCESS;
 }
