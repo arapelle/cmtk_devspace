5b96087c2642daec3133d8102652497de739cd09
Aymeric Pellé <aympelle@gmail.com>
2023-06-24 11:24:27 +0200

    v0.3:#17: cmtk_tests-cpp_texe: Add cpp_compute_odd_tests.cpp to add_cpp_executable_basic_tests() SOURCES.

--- projects/cmtk_tests/cmtk_tests-cpp_texe/test/cpp_compute_odd_tests.cpp
+++ projects/cmtk_tests/cmtk_tests-cpp_texe/test/cpp_compute_odd_tests.cpp
@@ -1,19 +1,26 @@
-#include <cmtk_tests/cpp_util/cpp_util.hpp>
+#include <cmtk_tests/cpp_texe/cpp_texe_main.hpp>
 #include <gtest/gtest.h>
 #include <cstdlib>
 
-TEST(cpp_compute_odd_tests, compute__7_11__77)
-{
-    ASSERT_EQ(cpp_util::compute(7, 11), 77);
-}
+std::string program_path;
 
-TEST(cpp_compute_odd_tests, compute__3_5__15)
+TEST(cpp_compute_even_tests, main__3_5_7__22)
 {
-    ASSERT_EQ(cpp_util::compute(3, 5), 15);
+    std::stringstream buffer;
+    std::streambuf* sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
+
+    std::array args = { program_path.c_str(), "3", "5", "7" };
+    cpp_texe::main(args.size(), const_cast<char**>(args.data()));
+
+    std::cout.rdbuf(sbuf);
+    std::string output_str = buffer.str();
+    ASSERT_EQ(output_str.find("[info] operation-0.1.0(3, 5, 7) = 22"), 0);
 }
 
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+    program_path = argv[0];
     return RUN_ALL_TESTS();
 }
