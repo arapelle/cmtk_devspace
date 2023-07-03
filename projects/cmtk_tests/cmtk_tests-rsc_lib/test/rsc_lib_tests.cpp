9f85b5339c3f03a7d3265dbfeef498f104cdb809
Aymeric Pellé <aympelle@gmail.com>
2023-07-03 16:08:55 +0200

    v0.3:#8: Add and improve unit tests in rsc_lib project.

--- projects/cmtk_tests/cmtk_tests-rsc_lib/test/rsc_lib_tests.cpp
+++ projects/cmtk_tests/cmtk_tests-rsc_lib/test/rsc_lib_tests.cpp
@@ -1,10 +1,22 @@
 #include <assets/assets.hpp>
 #include <gtest/gtest.h>
 
-TEST(rsc_lib_tests, resource__size__ok)
+TEST(rsc_lib_tests, resource_icon__path_and_size__ok)
 {
-    EXPECT_EQ(assets::grey_bird_path, "ASSETS:/birds/grey bird.jpg");
-    EXPECT_EQ(assets::grey_bird.size(), 6353336);
+    EXPECT_EQ(assets::icon_path, "ASSETS:/icon.png");
+    EXPECT_EQ(assets::icon.size(), 5465);
+}
+
+TEST(rsc_lib_tests, resource_tale__path_and_size__ok)
+{
+    EXPECT_EQ(assets::tale_path, "ASSETS:/text/tale.txt");
+    EXPECT_EQ(assets::tale.size(), 19);
+}
+
+TEST(rsc_lib_tests, resource_message__path_and_size__ok)
+{
+    EXPECT_EQ(assets::message_path, "ASSETS:/text/short-message/message.txt");
+    EXPECT_EQ(assets::message.size(), 6);
 }
 
 TEST(rsc_lib_tests, find_serialized_resource__tale__found)
@@ -13,12 +25,49 @@ TEST(rsc_lib_tests, find_serialized_resource__tale__found)
     std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
     EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
     EXPECT_EQ(rsc_bytes_o->size(), 19);
-    std::span rsc_bytes(*rsc_bytes_o);
-    std::string_view content(reinterpret_cast<const char*>(rsc_bytes.data()), rsc_bytes.size());
+    std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
     EXPECT_EQ(content, "Once upon a time...");
 }
 
-// resource_map()
+TEST(rsc_lib_tests, find_serialized_resource__another_tale__found)
+{
+    std::string_view rsc_path = "ASSETS:/text/another-tale.txt";
+    std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
+    EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->size(), 29);
+    std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
+    EXPECT_EQ(content, "Once upon a time, a knight...");
+}
+
+TEST(rsc_lib_tests, find_serialized_resource__third_tale__found)
+{
+    std::string_view rsc_path = "ASSETS:/text/third tale.txt";
+    std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
+    EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->size(), 27);
+    std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
+    EXPECT_EQ(content, "Once upon a time, a frog...");
+}
+
+TEST(rsc_lib_tests, find_serialized_resource__fourth_tale__found)
+{
+    std::string_view rsc_path = "ASSETS:/text/fourth_tale.txt";
+    std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
+    EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->size(), 29);
+    std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
+    EXPECT_EQ(content, "Once upon a time, a dragon...");
+}
+
+TEST(rsc_lib_tests, find_serialized_resource__message__found)
+{
+    std::string_view rsc_path = "ASSETS:/text/short-message/message.txt";
+    std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
+    EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->size(), 6);
+    std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
+    EXPECT_EQ(content, "coucou");
+}
 
 TEST(rsc_lib_tests, find_serialized_resource__trap__not_found)
 {
