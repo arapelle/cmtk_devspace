20a43b770c9d431320e29a1a2e86467a7bc66e18
Aymeric Pellé <aympelle@gmail.com>
2023-07-03 18:07:57 +0200

    v0.3:#8: Global resource variables are static variable in functions now.

--- projects/cmtk_tests/cmtk_tests-rsc_lib/test/rsc_lib_tests.cpp
+++ projects/cmtk_tests/cmtk_tests-rsc_lib/test/rsc_lib_tests.cpp
@@ -4,19 +4,19 @@
 TEST(rsc_lib_tests, resource_icon__path_and_size__ok)
 {
     EXPECT_EQ(assets::icon_path, "ASSETS:/icon.png");
-    EXPECT_EQ(assets::icon.size(), 5465);
+    EXPECT_EQ(assets::icon().size(), 5465);
 }
 
 TEST(rsc_lib_tests, resource_tale__path_and_size__ok)
 {
     EXPECT_EQ(assets::tale_path, "ASSETS:/text/tale.txt");
-    EXPECT_EQ(assets::tale.size(), 19);
+    EXPECT_EQ(assets::tale().size(), 19);
 }
 
 TEST(rsc_lib_tests, resource_message__path_and_size__ok)
 {
     EXPECT_EQ(assets::message_path, "ASSETS:/text/short-message/message.txt");
-    EXPECT_EQ(assets::message.size(), 6);
+    EXPECT_EQ(assets::message().size(), 6);
 }
 
 TEST(rsc_lib_tests, find_serialized_resource__tale__found)
@@ -24,7 +24,8 @@ TEST(rsc_lib_tests, find_serialized_resource__tale__found)
     std::string_view rsc_path = "ASSETS:/text/tale.txt";
     std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
     EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
-    EXPECT_EQ(rsc_bytes_o->size(), 19);
+    EXPECT_EQ(rsc_bytes_o->data(), assets::tale().data());
+    EXPECT_EQ(rsc_bytes_o->size(), assets::tale().size());
     std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
     EXPECT_EQ(content, "Once upon a time...");
 }
@@ -34,6 +35,7 @@ TEST(rsc_lib_tests, find_serialized_resource__another_tale__found)
     std::string_view rsc_path = "ASSETS:/text/another-tale.txt";
     std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
     EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->data(), assets::another_tale().data());
     EXPECT_EQ(rsc_bytes_o->size(), 29);
     std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
     EXPECT_EQ(content, "Once upon a time, a knight...");
@@ -44,6 +46,7 @@ TEST(rsc_lib_tests, find_serialized_resource__third_tale__found)
     std::string_view rsc_path = "ASSETS:/text/third tale.txt";
     std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
     EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->data(), assets::third_tale().data());
     EXPECT_EQ(rsc_bytes_o->size(), 27);
     std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
     EXPECT_EQ(content, "Once upon a time, a frog...");
@@ -54,6 +57,7 @@ TEST(rsc_lib_tests, find_serialized_resource__fourth_tale__found)
     std::string_view rsc_path = "ASSETS:/text/fourth_tale.txt";
     std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
     EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->data(), assets::fourth_tale().data());
     EXPECT_EQ(rsc_bytes_o->size(), 29);
     std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
     EXPECT_EQ(content, "Once upon a time, a dragon...");
@@ -64,6 +68,7 @@ TEST(rsc_lib_tests, find_serialized_resource__message__found)
     std::string_view rsc_path = "ASSETS:/text/short-message/message.txt";
     std::optional rsc_bytes_o = assets::find_serialized_resource(rsc_path);
     EXPECT_TRUE(static_cast<bool>(rsc_bytes_o));
+    EXPECT_EQ(rsc_bytes_o->data(), assets::message().data());
     EXPECT_EQ(rsc_bytes_o->size(), 6);
     std::string_view content(reinterpret_cast<const char*>(rsc_bytes_o->data()), rsc_bytes_o->size());
     EXPECT_EQ(content, "coucou");
