d30b4682248bed8fc294dc50a687b8d3a30a2556
Aymeric Pellé <aympelle@gmail.com>
2023-07-05 15:41:35 +0200

    v0.3:#8: Add includes in basic cmake project to test if they are well installed.

--- projects/cmtk_tests/cmtk_tests-rsc_lib/example/basic_cmake_project/main.cpp
+++ projects/cmtk_tests/cmtk_tests-rsc_lib/example/basic_cmake_project/main.cpp
@@ -2,6 +2,11 @@
 #include <cmtk_tests/rsc_lib/rsc_lib.hpp>
 #include <cmtk_tests/rsc_lib/version.hpp>
 #include <cmtk_tests/rsc_lib/icon.hpp>
+#include <cmtk_tests/rsc_lib/text/tale.hpp>
+#include <cmtk_tests/rsc_lib/text/another_tale.hpp>
+#include <cmtk_tests/rsc_lib/text/third_tale.hpp>
+#include <cmtk_tests/rsc_lib/text/fourth_tale.hpp>
+#include <cmtk_tests/rsc_lib/text/short_message/message.hpp>
 
 int main()
 {
