6eb640b5baa6aa1914440be612109a56aac305a0
Aymeric Pellé <aympelle@gmail.com>
2023-07-07 15:56:01 +0200

    v0.3:#8: Add cmtk_tests-rsc_lib and resorice to the test scripts.

--- test_build_install_cpp_projects.sh
+++ test_build_install_cpp_projects.sh
@@ -7,8 +7,9 @@ commandf "$format" rm -rf /tmp/local \
 && commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_hutil \
 && commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_util \
 && commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-alphabet \
-&& commandf "$format" cmake_test_full_build ./projects/cmtk_tests/cmtk_tests-rsc_lib \
+&& commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-rsc_lib \
 && commandf "$format" cmake_test_full_install ./projects/no_namespace/lymerenide \
+&& commandf "$format" cmake_test_full_install ./projects/no_namespace/resorice \
 && echo "SUCCESS" && bell -b && exit 0
 
 echo "FAIL"
