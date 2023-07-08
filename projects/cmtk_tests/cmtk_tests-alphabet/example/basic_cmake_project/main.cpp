e6ad7da0c6b87cbb3cafa54716eea71fc06b8f9f
Aymeric Pellé <aympelle@gmail.com>
2023-07-08 14:52:41 +0200

    v0.3.1: alphabet: Improve basic CMake example.

--- projects/cmtk_tests/cmtk_tests-alphabet/example/basic_cmake_project/main.cpp
+++ projects/cmtk_tests/cmtk_tests-alphabet/example/basic_cmake_project/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cmtk_tests/alphabet/version.hpp>
 #include <cmtk_tests/alpha/alpha.hpp>
+#include <cmtk_tests/alpha/version.hpp>
 #include <cmtk_tests/beta/beta.hpp>
+#include <spdlog/spdlog.h>
 
 int main()
 {
+    spdlog::set_level(spdlog::level::level_enum(SPDLOG_ACTIVE_LEVEL));
     std::cout << CMTK_TESTS_ALPHABET_VERSION << std::endl;
-    std::cout << alphabet::alpha::module_name() << std::endl;
+    std::cout << alphabet::alpha::module_name() << " " << CMTK_TESTS_ALPHA_VERSION << std::endl;
     std::cout << alphabet::beta::module_name() << std::endl;
     return EXIT_SUCCESS;
 }
