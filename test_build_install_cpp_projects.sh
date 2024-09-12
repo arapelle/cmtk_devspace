6e23a5e719a1c7e68ae9ff5cbf503e58b613b18f
Aymeric Pellé <aympelle@gmail.com>
2024-09-12 13:52:09 +0200

    WIP use loops.

--- test_build_install_cpp_projects.sh
+++ test_build_install_cpp_projects.sh
@@ -2,16 +2,26 @@
 
 format="{bold}{blue}"
 commandf "$format" rm -rf /tmp/local
-commandf "$format" cmake_test_install ./projects/cmtk_tests/cmtk_tests-cpp_exe \
-&& commandf "$format" cmake_test_full_build ./projects/cmtk_tests/cmtk_tests-cpp_texe \
-&& commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_hutil \
-&& commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_util \
-&& commandf "$format" cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-rsc_lib \
-&& commandf "$format" cmake_test_full_install ./projects/alphabet/alphabet-alphabet \
-&& commandf "$format" cmake_test_full_install ./projects/no_namespace/lymerenide \
-&& commandf "$format" cmake_test_full_install ./projects/no_namespace/resorice \
-&& echo "SUCCESS" && bell -b && exit 0
 
-echo "FAIL"
-tocsin -b
-exit 1
+project_cmds=(
+    "cmake_test_install      ./projects/cmtk_tests/cmtk_tests-cpp_exe"
+    "cmake_test_full_build   ./projects/cmtk_tests/cmtk_tests-cpp_texe"
+    "cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_hutil"
+    "cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-cpp_util"
+    "cmake_test_full_install ./projects/cmtk_tests/cmtk_tests-rsc_lib"
+    "cmake_test_full_install ./projects/no_namespace/lymerenide"
+    "cmake_test_full_install ./projects/no_namespace/resorice"
+)
+
+for ((i = 0; i < ${#project_cmds[@]}; ++i))
+do
+    commandf "$format" ${project_cmds[$i]}
+    if (($? != 0))
+    then
+        echo "FAIL"
+        tocsin -b
+        exit 1
+    fi
+done
+
+echo "SUCCESS" && bell -b && exit 0
